a756bd3b4cc0503e11c7f1fc10a02a12d6d3557d
vicmerca <vicmerca@iu.edu>
2020-08-31 21:36:05 -0400

    edited main.py and main2.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import sys
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+number = 5
+guess = input("Guess a number from 1 to 10: ")
+guess = int(guess)
+if guess == number:
+    print("Great job! You got it!")
+else:
+    print("Sorry, better luck next time.")
+    print("The number was " + str(number))
--- main2.py
+++ main2.py
@@ -1,2 +1,14 @@
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+import random
+number = random.randint(0,10)
+approval = 0
+while approval != 1:
+    guess = input("Guess a number from 1 to 10: ")
+    if int(guess) != number:
+        print("Try again")
+    elif int(guess) == number:
+        approval = 1
+        print("Great job! You got it!")
+    else:
+        print("That was not a number...")
